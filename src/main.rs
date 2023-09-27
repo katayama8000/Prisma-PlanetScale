d0df91903bd59c83ef69d86bdcca06e644b8c75b
katayama8000 <tattu.0310@gmail.com>
2023-09-27 11:44:06 +0900

    route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let routers_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("hello<strong>world</strong>") }),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(routers_hello.into_make_service())
+        .await
+        .unwrap()
 }
