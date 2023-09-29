11eb2942faf0ef07469396a1bf045c09426adc5c
katayama8000 <tattu.0310@gmail.com>
2023-09-29 23:39:34 +0900

    mutex

---
+++ src/model.rs
@@ -0,0 +1,29 @@
+use crate::{Error, Result};
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug, Serialize, Clone)]
+pub struct Ticket {
+    pub id: u64,
+    pub title: String,
+}
+
+#[derive(Deserialize)]
+pub struct TicketForCreate {
+    pub title: String,
+}
+
+#[derive(Clone)]
+pub struct ModelController {
+    tickets_store: Arc<Mutex<Vec<Option<Ticket>>>>,
+}
+
+impl ModelController {
+    pub async fn new() -> Result<Self> {
+        Ok(Self {
+            tickets_store: Arc::default(),
+        })
+    }
+}
+
+// 29分ごろから
