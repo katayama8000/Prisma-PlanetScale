be19e1461f938fbd3c88ee5b132c3684a348cf6c
katayama8000 <tattu.0310@gmail.com>
2023-10-07 12:17:24 +0900

    ctx

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,10 +1,16 @@
+use async_trait::async_trait;
+use axum::extract::{FromRequestParts, State};
+use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum::RequestPartsExt;
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
+use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
+
 use crate::{Error, Result};
 
 pub async fn mw_requier<B>(cookies: Cookies, req: Request<B>, next: Next<B>) -> Result<Response> {
@@ -31,4 +37,21 @@ fn parse_token(token: String) -> Result<(u64, String, String)> {
         .map_err(|_| Error::AuthFailTokenWrongFormat)?;
 
     Ok((user_id, exp.to_string(), sign.to_string()))
+}
+
+#[async_trait]
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx", "EXTRACTOR");
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.to_string());
+
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthToken)
+            .and_then(parse_token)?;
+        Ok(Ctx::new(user_id))
+    }
 }
